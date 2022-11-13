c1efab2f4fec6b1872e757dee1eee6535ed19e7e
Fourat-Lahmar <79519720+Fourat-Lahmar@users.noreply.github.com>
2022-11-13 13:35:52 +0100

    Update ProduitServiceTest.java

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -1,14 +1,24 @@
 package tn.esprit.rh.achat.services;
 
-import org.junit.jupiter.api.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.rh.achat.entities.Produit;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -22,13 +32,9 @@ public class ProduitServiceTest {
     @Autowired
     IProduitService ps;
 
-    @Test
-    @Order(1)
-    public void testRetrieveAllUsers() {
-        List<Produit> listProduits = ps.retrieveAllProduits();
-        Assertions.assertEquals(0, listProduits.size());
-    }
-    @Test
+
+   @Order(1)
+	@Test
     public void addProduitWithSuccess() throws ParseException{
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
         Date dateCreation = dateFormat.parse("30/08/2021");
@@ -45,7 +51,26 @@ public class ProduitServiceTest {
         assertNotNull(produit.getPrix());
         assertTrue(produit.getLibelleProduit().length() > 0 );
     }
-    
+    @Order(2)
+	@Test
+     public void deleteClientWithSuccess() throws ParseException{
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date dateCreation = dateFormat.parse("30/08/2021");
+        Date dateDerniereModification =dateFormat.parse("30/09/2021");
+        Produit produit = new Produit();
+        produit.setPrix(11);
+        produit.setDateCreation(dateCreation);
+        produit.setDateDerniereModification(dateDerniereModification);
+        produit.setLibelleProduit("libelleProduit");
+        produit.setCodeProduit("code2212222");
+        Produit p = service.addProduit(produit);
+        service.deleteProduit(p.getIdProduit());
+        assertNull(service.retrieveProduit(p.getIdProduit()));
+
+
+    }
+    @Order(3)
+	@Test
     public void retrieveAllProduits() throws ParseException{
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date dateCreation = dateFormat.parse("30/08/2021");
